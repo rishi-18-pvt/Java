373eaa6609073c2c7604cdf4c4ade0d7560fc1ca
Rishi R <rishirreddy18@gmail.com>
2025-07-24 14:52:53 +0000

    Array length

---
+++ Arrays/ArrayLength.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class ArrayLength {
+    static int ArrayLength(int[]arr){
+        return arr.length;
+    }
+    public static void main(String[] args) {
+        int[] arr ={12,14,5,6,8};
+        System.out.println(ArrayLength(arr));
+    }
+    
+}
